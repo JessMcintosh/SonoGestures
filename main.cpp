9107481bddbfcfd4166aa4e436e16191e668d27c
JessMcintosh <jm0152@my.bristol.ac.uk>
2016-02-26 20:23:39 +0000

    OpenCV base program

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    cv::VideoCapture cap(argv[1]);
+    if (!cap.isOpened())
+    {
+        std::cout << "!!! Failed to open file: " << argv[1] << std::endl;
+        return -1;
+    }
+
+    cv::Mat frame;
+    for(;;)
+    {
+
+        if (!cap.read(frame))             
+            break;
+
+        cv::imshow("window", frame);
+
+        char key = cvWaitKey(10);
+        if (key == 27) // ESC
+            break;
+    }
+
+    return 0;
+}
