03fbb909e7c2e2687856e34912a4f3250a146d13
Jess McIntosh <jm0152@my.bristol.ac.uk>
2016-02-27 13:53:47 +0000

    optical flow code

--- main.cpp
+++ main.cpp
@@ -1,30 +1,99 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
 
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
-    cv::VideoCapture cap(argv[1]);
-    if (!cap.isOpened())
-    {
-        std::cout << "!!! Failed to open file: " << argv[1] << std::endl;
-        return -1;
-    }
+using namespace cv;
+using namespace std;
 
-    cv::Mat frame;
+//int main(int argc, char* argv[])
+//{
+//    cv::VideoCapture cap(argv[1]);
+//    if (!cap.isOpened())
+//    {
+//        std::cout << "!!! Failed to open file: " << argv[1] << std::endl;
+//        return -1;
+//    }
+//
+//    cv::Mat frame;
+//	cv::Mat of_frame;
+//    for(;;)
+//    {
+//
+//        if (!cap.read(frame))             
+//            break;
+//
+//        cv::imshow("window", frame);
+//
+//		cv::calcOpticalFlowFarneback(frame, of_frame, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma, int flags)
+//
+//        char key = cvWaitKey(10);
+//        if (key == 27) // ESC
+//            break;
+//    }
+//
+//    return 0;
+//}
+
+
+static void help()
+{
+    cout <<
+            "\nThis program demonstrates dense optical flow algorithm by Gunnar Farneback\n"
+            "Mainly the function: calcOpticalFlowFarneback()\n"
+            "Call:\n"
+            "./fback\n"
+            "This reads from video camera 0\n" << endl;
+}
+static void drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step,
+                    double, const Scalar& color)
+{
+    for(int y = 0; y < cflowmap.rows; y += step)
+        for(int x = 0; x < cflowmap.cols; x += step)
+        {
+            const Point2f& fxy = flow.at<Point2f>(y, x);
+            line(cflowmap, Point(x,y), Point(cvRound(x+fxy.x), cvRound(y+fxy.y)),
+                 color);
+            circle(cflowmap, Point(x,y), 2, color, -1);
+        }
+}
+
+int main(int argc, char** argv)
+{
+    cv::CommandLineParser parser(argc, argv, "{help h||}");
+    if (parser.has("help"))
+    {
+        help();
+        return 0;
+    }
+    VideoCapture cap(argv[1]);
+    help();
+    if( !cap.isOpened() )
+        return -1;
+
+    Mat flow, cflow, frame;
+    UMat gray, prevgray, uflow;
+    namedWindow("flow", 1);
+
     for(;;)
     {
+        cap >> frame;
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
 
-        if (!cap.read(frame))             
+        if( !prevgray.empty() )
+        {
+            calcOpticalFlowFarneback(prevgray, gray, uflow, 0.5, 3, 15, 3, 5, 1.2, 0);
+            cvtColor(prevgray, cflow, COLOR_GRAY2BGR);
+            uflow.copyTo(flow);
+            drawOptFlowMap(flow, cflow, 16, 1.5, Scalar(0, 255, 0));
+            imshow("flow", cflow);
+        }
+        if(waitKey(30)>=0)
             break;
-
-        cv::imshow("window", frame);
-
-        char key = cvWaitKey(10);
-        if (key == 27) // ESC
-            break;
+        std::swap(prevgray, gray);
     }
-
     return 0;
 }
+
