89556b89472cd007d3d9a200058fb6610dab7d04
JessMcintosh <jm0152@my.bristol.ac.uk>
2016-03-14 10:38:20 +0000

    align graphs

--- plotsplit.py
+++ plotsplit.py
@@ -8,6 +8,8 @@ import os
 import subprocess
 
 def splitvideos():
+
+    return
     splitPoints.sort()
     print splitPoints
     videoDir = os.path.dirname(videoFile)
@@ -15,8 +17,6 @@ def splitvideos():
     programPath = os.path.join(parentPath, 'SplitClips')
     print programPath
 
-    gestures = ('thumb','index','middle','ring','pinky','fist')
-    numGestPerformed = 5
 
     if videoDir != '':
         os.chdir(videoDir)
@@ -42,34 +42,54 @@ def splitvideos():
         currentIteration += 1
         subprocess.call([programPath, videoFile, str(splitPoints[i]), str(splitPoints[i+1]), outfile])
 
-
-
 def onclick(event):
-    global initX, initY
-    
+    global initX, initY, drag
+
     initX = event.xdata
     initY = event.ydata
+    if toggleClick is False:
+        drag = True
+    if toggleAlign is True:
+        drag = True
+        
+
 
 def onrelease(event):
-    global initX, initY
-    print 'xdata=%f'%(
-        event.xdata)
+    global initX, initY, splitLines, drag
+    #print 'xdata=%f'%(event.xdata)
 
-    if initX == event.xdata and initY == event.ydata:
-        #circ = plt.Circle((event.xdata, event.ydata), radius=2, color='g')
-        #ax1.add_patch(circ)
-        ln = plt.plot([event.xdata, event.xdata], [1000, -1000], color='k', linestyle='-', linewidth=2)
-        #number of frames per video segment is 250 (50frames * 5s)
-        #range to 250*numClips
-        #assuming only 10 clips.. 2500
-        for i in xrange(0,2500,250):
-            ln = plt.plot([event.xdata+i, event.xdata+i], [1000, -1000], color='k', linestyle='-', linewidth=2)
+    drag = False
+
+    return
+
+def motion(event):
+    global initX, initY, splitLines, drag
+
+    if drag is True:
+
+        dx =  event.xdata - initX
+        dy =  event.ydata - initY
+
+        if toggleAlign is False:
+
+            #number of frames per video segment is 250 (50frames * 5s)
+            #range to 250*numClips
+            #assuming only 10 clips.. 2500
+            for line in splitLines:
+                x = line.get_xdata()
+                y = line.get_ydata()
+                line.set_data(x+dx, y+dy)
+        else:
+            xdata = sensorLine.get_xdata()
+            ydata = sensorLine.get_ydata()
+            sensorLine.set_data(xdata+dx, ydata+dy)
+
+        initX = event.xdata
+        initY = event.ydata
         fig.canvas.draw()
-        splitPoints.append(int(round(initX)))
-        splitLines.append(ln)
 
 def press(event):
-    global cid, toggleClick
+    global cid, toggleClick, toggleAlign
     #print('press', event.key)
     sys.stdout.flush()
 
@@ -86,31 +106,60 @@ def press(event):
     # toggle click
     if event.key == ' ':
         if toggleClick:
-            cid = fig.canvas.mpl_connect('button_press_event', onclick)
+            #cid = fig.canvas.mpl_connect('button_press_event', onclick)
             print 'point selection mode on'
             toggleClick = False
         else:
-            fig.canvas.mpl_disconnect(cid)
+            #fig.canvas.mpl_disconnect(cid)
             print 'navigation mode on'
             toggleClick = True
+            toggleAlign = False
+    if event.key == 'a':
+        if toggleAlign:
+            toggleAlign = False
+            toggleClick = True
+        else:
+            print 'aligning second graph'
+            toggleAlign = True
+            toggleClick = False
 
 #videoFile = sys.argv[1] 
 graphFile = sys.argv[1] 
 
-toggleClick = False
+sensorFile = sys.argv[2]
+
+gestures = ('thumb','index','middle','ring','fist','point','call','gun','flex','adduct')
+numGestPerformed = 5
+
+toggleClick = True
+toggleAlign = False
+drag = False
 
 splitPoints = []
 splitLines = []
 
 data = pylab.loadtxt(graphFile)
+sensorData = pylab.loadtxt(sensorFile)
 
 fig = plt.figure()
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 rid = fig.canvas.mpl_connect('button_release_event', onrelease)
 pid = fig.canvas.mpl_connect('key_press_event', press)
+mid = fig.canvas.mpl_connect('motion_notify_event', motion)
+
 
 ax1 = fig.add_subplot(111)
 
 ax1.plot(data)
+
+sensorLine, = ax1.plot(sensorData)
+
+
+numLines = len(gestures)*numGestPerformed*250+250
+for i in xrange(0,numLines,250):
+    ln, = plt.plot([i, i], [300, -50], color='k', linestyle='-', linewidth=2)
+    splitLines.append(ln)
+#splitPoints.append(int(round(initX)))
+
 plt.show()
 
